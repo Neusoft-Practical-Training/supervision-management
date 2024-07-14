fd9ece2dc22dfbd113e1729a0e3e26b369aa17ab
Aftermath <From_JingWANG@outlook.com>
2024-07-14 18:57:06 +0800

    chore: dependency and some minor changes

--- src/api/index.ts
+++ src/api/index.ts
@@ -6,6 +6,9 @@ import { type Result } from './entities/result'
 import { useUserStore } from "@/stores/modules/user";
 
 import { type UserDTO } from './entities/user';
+import type { SearchGridMemberCondition } from '@/views/GridMemberMngView.vue'
+import type { SearchSupervisorCondition } from '@/views/SupervisorMngView.vue'
+import type { SearchAdminCondition } from '@/views/AdminMngView.vue'
 
 const baseURL = HTTP_HOST
 // const baseURL = 'http://127.0.0.1:8080'
@@ -58,3 +61,30 @@ export default instance
 
 export const login = async (data: { username: string; password: string }) =>
   (await instance.post<UserDTO>('/login', data)).data
+
+// 获得辖区内所有的网格员
+export const getGridMembers = async (adminId: number) =>
+  (await instance.get<UserDTO[]>(`/getGridMembers/${ adminId }`)).data
+
+// 获得辖区内所有的监督员
+export const getSupervisors = async (adminId: number) =>
+  (await instance.get<UserDTO[]>(`/getSupervisors/${ adminId }`)).data
+
+// 获得辖区内所有的管理员
+export const getAdmins = async (adminId: number) =>
+  (await instance.get<UserDTO[]>(`/getAdmins/${ adminId }`)).data
+
+export const selectGridMembers = async (data: {adminId: number, condition: SearchGridMemberCondition}) =>
+  (await instance.post<UserDTO[]>('/selectGridMembers', data)).data
+
+export const setUserStatus = async (userId: number) =>
+  (await instance.get<boolean>(`/setUserStatus/${ userId }`)).data
+
+export const setUserRemarks = async (data: {userId: number, remarks: string}) =>
+  (await instance.post<boolean>('/setUserRemarks', data)).data
+
+export const selectSupervisors = async (data: {adminId: number, condition: SearchSupervisorCondition}) =>
+  (await instance.post<UserDTO[]>('/selectSupervisors', data)).data
+
+export const selectAdmins = async (data: {adminId: number, condition: SearchAdminCondition}) =>
+  (await instance.post<UserDTO[]>('/selectAdmins', data)).data
--- src/common/constants.ts
+++ src/common/constants.ts
@@ -1,1 +1,2 @@
 export const HTTP_HOST = 'http://localhost:8080'
+export const BASE_AVATAR_URL = 'src/assets/base-avatar.jpeg'
