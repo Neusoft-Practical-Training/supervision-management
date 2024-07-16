9c702c02a36083213b5266f5ecb0159691e2a2ea
Aftermath <From_JingWANG@outlook.com>
2024-07-16 15:23:42 +0800

    feat: add some utils

--- src/util/format/formatEnum.ts
+++ src/util/format/formatEnum.ts
@@ -1,55 +1,112 @@
-import { AdminPermission, AqiFeedbackState, Gender, GridMemberState, Role, TaskCompletedState } from '@/common/enums'
+import {
+  AdminPermission,
+  AqiFeedbackState,
+  Gender,
+  GridMemberState,
+  LeaveRequestState,
+  Role,
+  TaskCompletedState
+} from '@/common/enums'
 
 export const formatGender = (gender: Gender): string => {
   switch (gender) {
-    case Gender.Female: return "女";
-    case Gender.Male: return "男";
-    case Gender.Unknown: return "未知";
+    case Gender.Female:
+      return '女'
+    case Gender.Male:
+      return '男'
+    case Gender.Unknown:
+      return '未知'
+    default:
+      return '未知状态'
   }
 }
 
 export const formatGridMemberState = (gridMemberState: GridMemberState) => {
   switch (gridMemberState) {
-    case GridMemberState.AwaitingReview: return "等待审核";
-    case GridMemberState.Working: return "工作中";
-    case GridMemberState.Idle: return "空闲";
-    case GridMemberState.Other: return "其他";
-    case GridMemberState.InVacation: return "休假中";
-    case GridMemberState.TemporaryTransfer: return "临时抽调";
+    case GridMemberState.AwaitingReview:
+      return '等待审核'
+    case GridMemberState.Working:
+      return '工作中'
+    case GridMemberState.Idle:
+      return '空闲'
+    case GridMemberState.Other:
+      return '其他'
+    case GridMemberState.InVacation:
+      return '休假中'
+    case GridMemberState.TemporaryTransfer:
+      return '临时抽调'
+    default:
+      return '未知状态'
   }
 }
 
 export const formatRole = (role: Role): string => {
   switch (role) {
-    case Role.Admin: return "管理员";
-    case Role.Supervisor: return "监督员";
-    case Role.GridMember: return "网格员";
+    case Role.Admin:
+      return '管理员'
+    case Role.Supervisor:
+      return '监督员'
+    case Role.GridMember:
+      return '网格员'
+    default:
+      return '未知状态'
   }
 }
 
 export const formatAdminPermission = (permission: AdminPermission): string => {
   switch (permission) {
-    case AdminPermission.State: return "国家级";
-    case AdminPermission.Province: return "省级";
-    case AdminPermission.City: return "市级";
+    case AdminPermission.State:
+      return '国家级'
+    case AdminPermission.Province:
+      return '省级'
+    case AdminPermission.City:
+      return '市级'
+    default:
+      return '未知状态'
   }
 }
 
 export const formatAqiFeedbackState = (state: AqiFeedbackState): string => {
   switch (state) {
-    case AqiFeedbackState.Unassigned: return "未指派";
-    case AqiFeedbackState.Assigned: return "已指派";
-    case AqiFeedbackState.Completed: return "已完成";
+    case AqiFeedbackState.Unassigned:
+      return '未指派'
+    case AqiFeedbackState.Assigned:
+      return '已指派'
+    case AqiFeedbackState.Completed:
+      return '已完成'
+    default:
+      return '未知状态'
   }
 }
 
 export const formatTaskCompletedState = (state: TaskCompletedState): string => {
   switch (state) {
-    case TaskCompletedState.Uncompleted: return "未完成";
-    case TaskCompletedState.Completed: return "已完成";
-    case TaskCompletedState.CrossDomainRequesting: return "跨域请求中";
-    case TaskCompletedState.CrossDomainRequestDenied: return "跨域请求被拒";
-    case TaskCompletedState.CrossDomainRequestAccepted: return "跨域请求被接受";
-    case TaskCompletedState.CrossDomainRequestCompleted: return "跨域请求完成";
+    case TaskCompletedState.Uncompleted:
+      return '未完成'
+    case TaskCompletedState.Completed:
+      return '已完成'
+    case TaskCompletedState.CrossDomainRequesting:
+      return '跨域请求中'
+    case TaskCompletedState.CrossDomainRequestDenied:
+      return '跨域请求被拒'
+    case TaskCompletedState.CrossDomainRequestAccepted:
+      return '跨域请求被接受'
+    case TaskCompletedState.CrossDomainRequestCompleted:
+      return '跨域请求完成'
+    default:
+      return '未知状态'
+  }
+}
+
+export const formatLeaveRequestState = (state: LeaveRequestState): string => {
+  switch (state) {
+    case LeaveRequestState.Pending:
+      return '待审批'
+    case LeaveRequestState.Approved:
+      return '批准'
+    case LeaveRequestState.Rejected:
+      return '拒绝'
+    default:
+      return '未知状态'
   }
 }
--- src/util/format/formatTime.ts
+++ src/util/format/formatTime.ts
@@ -9,8 +9,30 @@ export const formatUseTime = (createTime: string): string => {
   const diffInMs = currentDate.getTime() - startDate.getTime();
 
   // 将毫秒差转换为天数
-  const diffInDays = Math.floor(diffInMs / (1000 * 60 * 60 * 24));
+  const diffInDays = Math.max(Math.floor(diffInMs / (1000 * 60 * 60 * 24)), 0);
 
   // 返回天数差，根据需要可以转换为字符串或保持为数字
   return diffInDays.toString();
+}
+
+export const formatDate = (date: Date): string => {
+  const year = date.getFullYear();
+  const month = date.getMonth() + 1;  // getMonth() 返回的是 0-11，所以需要加1
+  const day = date.getDate();
+
+  // 使用模板字符串来格式化，确保月份和日期为两位数
+  return `${year}-${month.toString().padStart(2, '0')}-${day.toString().padStart(2, '0')}`;
+}
+
+/**
+ * formatLeaveDuration
+ * @param startTime - The start time of the leave in ISO string format
+ * @param endTime - The end time of the leave in ISO string format
+ * @returns The duration of the leave in days
+ */
+export function formatLeaveDuration(startTime: string, endTime: string): string {
+  const start = new Date(startTime);
+  const end = new Date(endTime);
+  const duration = Math.ceil((end.getTime() - start.getTime()) / (1000 * 60 * 60 * 24));
+  return `${duration} 天`;
 }
