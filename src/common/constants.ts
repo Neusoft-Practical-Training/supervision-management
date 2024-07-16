b97d453577227361db171e301312020e9d658828
Aftermath <From_JingWANG@outlook.com>
2024-07-16 15:25:09 +0800

    feat: add some commons for project

--- src/common/constants.ts
+++ src/common/constants.ts
@@ -1,2 +1,5 @@
 export const HTTP_HOST = 'http://localhost:8080'
 export const BASE_AVATAR_URL = 'src/assets/base-avatar.jpeg'
+export const SECURITY_JS_CODE_AMAP = "e1c90387222ed87a6586cc22f7693a4c"
+export const KEY_AMAP = "8d92ff0e0a881a3bb7db6bae8f760464"
+export const VERSION_AMAP = "2.0"
